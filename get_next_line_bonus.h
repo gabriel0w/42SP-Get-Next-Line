5327c1dd4e8758949192ba7189ff03414664d29f
Gabriel0w <gabriel0w@gabrielo.dzn@gmail.com>
2021-11-12 19:14:51 -0300

    Working and clean

---
+++ get_next_line_bonus.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define MAX_FD 256
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_substr(char const *s, size_t start, size_t len);
+size_t	ft_strlen(const char *string);
+char	*ft_strdup(const char *src);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+
+#endif
